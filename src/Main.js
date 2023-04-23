28afe54e569647e93b1c5a2c356a46fc5272e9c3
Dheeraj <6d6369@gmail.com>
2023-04-23 14:12:23 +0530

    BrowserRouter link adding

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,18 @@ import Navbar from "./components/Navbar";
 import "./Main.css";
 import Home from "./components/Home";
 import Works from "./components/Works";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function Main() {
   return (
     <div id="Main-container">
-      <Navbar />
-      <Home />
-      <Works/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/work" element={<Works />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './styles/Navbar.css'
 function Navbar() {
   return (
@@ -8,8 +9,9 @@ function Navbar() {
         <h6>Developer</h6>
       </div>
       <nav className='nav-menu'>
-        <ul>
-          <li><a href="#Work" className='nav-link hover-style' >Work</a></li>
+        <ul>   
+          <li><Link to="/" className='nav-link hover-style'>Home</Link></li>
+          <li><Link to="work" className='nav-link hover-style'>Work</Link></li>
           <li><a href="#Exp" className='nav-link hover-style' >Experience</a></li>
           <li><a href="#Exp" className='nav-link hover-style' >About</a></li>
           <li><a href="#Contact" className='button' >Contact</a></li>
